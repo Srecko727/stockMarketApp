5196a25796292c69294b8d4ab922d5be03639201
Srecko727 <sreckomarkovic@ufl.edu>
2022-01-07 00:30:50 -0500

    listing stock traits

--- stock/quotes/views.py
+++ stock/quotes/views.py
@@ -3,8 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    import requests
+    import json
     # pk_3b2b7dea6591476892c2c05fed85979e
-    return render(request, 'home.html', {})
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_3b2b7dea6591476892c2c05fed85979e")
+
+    try: 
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+
+    return render(request, 'home.html', {'api':api})
 
 def about(request):
     return render(request, 'about.html', {})
