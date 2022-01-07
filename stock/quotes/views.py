d580a9efcf5b2a3bc05ccc01bf067c3585359bb7
Srecko727 <sreckomarkovic@ufl.edu>
2022-01-07 00:56:22 -0500

    look up any stock to find details listed

--- stock/quotes/views.py
+++ stock/quotes/views.py
@@ -5,16 +5,20 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
-    # pk_3b2b7dea6591476892c2c05fed85979e
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_3b2b7dea6591476892c2c05fed85979e")
 
-    try: 
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_3b2b7dea6591476892c2c05fed85979e")
 
+        try: 
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'home.html', {'api':api})
 
-    return render(request, 'home.html', {'api':api})
+    else:
+        return render(request, 'home.html', {'ticker':"Enter A Ticker Symbol Above..."})
+
 
 def about(request):
     return render(request, 'about.html', {})
