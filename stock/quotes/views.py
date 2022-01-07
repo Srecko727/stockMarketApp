a15de29ad3c9be64e5b9710c7e6d2dd72ef8da00
Srecko727 <sreckomarkovic@ufl.edu>
2022-01-07 00:00:32 -0500

    stock api

--- stock/quotes/views.py
+++ stock/quotes/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    # pk_3b2b7dea6591476892c2c05fed85979e
     return render(request, 'home.html', {})
 
 def about(request):
