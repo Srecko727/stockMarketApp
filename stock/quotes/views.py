48d5a064698e668f171893350c8aaee317c83cf5
Srecko727 <sreckomarkovic@ufl.edu>
2022-01-11 19:34:34 -0500

    connected api to table and made it so i can add and delete stocks from it

--- stock/quotes/views.py
+++ stock/quotes/views.py
@@ -30,6 +30,8 @@ def about(request):
     return render(request, 'about.html', {})
 
 def add_stock(request):
+    import requests
+    import json
     if request.method == 'POST':
         form = StockForm(request.POST or None)
         if form.is_valid():
@@ -38,10 +40,24 @@ def add_stock(request):
             return redirect('add_stock')
     else:
         ticker = Stock.objects.all()
-        return render(request, 'add_stock.html', {'ticker': ticker })
+        output = []
+        for ticker_item in ticker:
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(ticker_item)  + "/quote?token=pk_3b2b7dea6591476892c2c05fed85979e")
+            try: 
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Error..."
+
+        return render(request, 'add_stock.html', {'ticker': ticker, 'output': output })
 
 def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
     item.delete()
-    messages.success(request, ("Stock has Successfully been Deleted"))
-    return redirect(add_stock)
+    messages.success(request, ( str(item) + " has Successfully been Deleted"))
+    return redirect(delete_stock)
+
+
+def delete_stock(request):
+    ticker = Stock.objects.all()
+    return render(request, 'delete_stock.html', {'ticker': ticker})
